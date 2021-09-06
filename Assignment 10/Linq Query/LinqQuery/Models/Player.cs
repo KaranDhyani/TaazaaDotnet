6d6501c5966d4ff334e49fafe5250edaaee65d7a
Karan Dhyani <5999.karandhyani@gmail.com>
2021-09-06 11:30:59 +0530

    SQL

---
+++ Assignment 10/Linq Query/LinqQuery/Models/Player.cs
@@ -0,0 +1,11 @@
+namespace LinqQuery.Models
+{
+    public class Player
+    {
+        public int ID{get;set;}
+
+        public string Name{get;set;}
+
+        public int age{get;set;}
+    }
+}
--- Assignment 10/Linq Query/LinqQuery/Program.cs
+++ Assignment 10/Linq Query/LinqQuery/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LinqQuery.Models;
 
 namespace LinqQuery
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<Player> list = new List<Player>();
+            {
+                new Player()
+                {
+                    ID = 100,
+                    Name = "Karan",
+                    age = 21
+                };
+                new Player()
+                {
+                    ID = 103,
+                    Name = "Aman",
+                    age = 22
+                };
+                new Player()
+                {
+                    ID = 111,
+                    Name = "Rohit",
+                    age = 25
+                };
+
+            };
+
+            var number = from s in list
+                         where s.age>21 && s.age<23
+                         select s;
+        
+            foreach(var str in number)
+            {
+                Console.WriteLine(str.Name);
+            }
+
         }
     }
 }
